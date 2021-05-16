1e56b2e050825972cc3cef23fd4d12ea4c7cbcbb
Evgeniy Om <evgenicuss@gmail.com>
2021-05-16 19:09:37 +0300

    добавил данные общего состояния счета в preloadedState

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
+const reducer = {
+  counter: counterReducer,
+}
+
+const preloadedState = {
+  total: {
+    cost: {
+      rub: 343073.13,
+      usd: 4635.34,
+    },
+    profit: {
+      rub: 35773.62,
+      percent: 10.43,
+    },
+    annualRateOfReturn: 27.54,
+    changesPerDay: {
+      rub: 728.78,
+      percent: 0.23,
+    }
   },
+  assets: {
+  }
+}
+export const store = configureStore({
+  reducer
 });
